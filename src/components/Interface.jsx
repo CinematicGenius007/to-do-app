eab0b79db5cb64a821716018d2fe510e7570e2b3
CinematicGenius007 <75163638+CinematicGenius007@users.noreply.github.com>
2022-12-24 20:23:57 +0530

    Update Interface.jsx

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -20,7 +20,7 @@ const Interface = () => {
     });
 
     const refreshTaskList = () => {
-        axios.get('http://ec2-18-212-192-194.compute-1.amazonaws.com/api/tasks', {
+        axios.get(`{process.env.REACT_APP_BACKEND_URL}api/tasks`, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Authorization': 'Bearer ' + document.cookie.split('=')[1],
@@ -40,7 +40,7 @@ const Interface = () => {
 
     const addNewTask = () => {
         if (newTask !== "") {
-            axios.post('http://ec2-18-212-192-194.compute-1.amazonaws.com/api/tasks/create', {
+            axios.post(`{process.env.REACT_APP_BACKEND_URL}api/tasks/create`, {
                 title: newTask,
                 time_created: moment().format('YYYY-MM-DD HH:mm:ss'),
             }, {
@@ -64,7 +64,7 @@ const Interface = () => {
     };
 
     const changeTaskStatus = (id, status = 1) => {
-        axios.post('http://ec2-18-212-192-194.compute-1.amazonaws.com/api/tasks/updateStatus', {
+        axios.post(`{process.env.REACT_APP_BACKEND_URL}api/tasks/updateStatus`, {
             task_id: id,
             status: status,
         }, {
@@ -86,7 +86,7 @@ const Interface = () => {
     };
 
     const deleteTask = (id) => {
-        axios.delete('http://ec2-18-212-192-194.compute-1.amazonaws.com/api/tasks/delete', {
+        axios.delete(`{process.env.REACT_APP_BACKEND_URL}api/tasks/delete`, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Authorization': 'Bearer ' + document.cookie.split('=')[1],
@@ -126,7 +126,7 @@ const Interface = () => {
                 title: null,
             });
         } else if (newTask !== "") {
-            axios.put('http://ec2-18-212-192-194.compute-1.amazonaws.com/api/tasks/update', {
+            axios.put(`{process.env.REACT_APP_BACKEND_URL}api/tasks/update`, {
                 task_id: updateState.id,
                 title: newTask,
             }, {
